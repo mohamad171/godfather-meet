f6171ce6c4c14836128fe1ac7a8583507ba1b9fc
Mohamad <mohamadmohamadi249@gmail.com>
2024-04-13 09:46:57 +0330

    join room request

--- backend/godfather_api_interface.js
+++ backend/godfather_api_interface.js
@@ -6,11 +6,12 @@ let baseUrl = "https://godfathergame.ir/api/v1/games"
 module.exports.joinPlayer = function(token,roomCode,action){
     axios.post(`${baseUrl}/join?secret=${godfatherSecretKey}`,{
         "token":token,"room_code":roomCode,"action":action
-    }).then((result)=>{
-        console.log(result)
-        return true;
-    }).catch((result)=>{
-        console.log(result)
-        return false;
+    }).then((response)=>{
+        return response.data
+    }).catch((error)=>{
+        if (error.response){
+            return error.response.data
+        }
+
     })
 }
