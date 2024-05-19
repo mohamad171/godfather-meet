858cb2b6270d960f556dcd17b5d52a827598a841
Mohamad Mohamadi <mohamadmohamadi249@gmail.com>
2024-05-19 18:28:11 +0330

    socket

--- backend/server.js
+++ backend/server.js
@@ -46,17 +46,15 @@ signalServer.on('discover', async (request) => {
             request.discover({
                 peers: Array.from(members)
             });
-            if(response.data.data["room_role"] === "god"){
+            if (response.data.data["room_role"] === "god") {
                 request.socket.join(`god-${roomId}`);
-            }
-
-            else if(response.data["data"]["role"]){
-                if(response.data["data"]["role"]["side"] === 0){
+            } else if (response.data["data"]["role"]) {
+                if (response.data["data"]["role"]["side"] === 0) {
                     console.log("Add To mafia room")
                     request.socket.join(`mafia-${roomId}`);
                 }
             }
-            console.log("User id:",response.data.data["player"]["id"])
+            console.log("User id:", response.data.data["player"]["id"])
             request.socket.join(`${response.data.data["player"]["id"]}`);
 
             io.to(memberId).emit("join_game", {"status": true, "data": response.data})
@@ -82,10 +80,10 @@ signalServer.on('disconnect', (socket) => {
         members.delete(memberId)
     }
     axios.post(`${baseUrl}/leave?secret=${godfatherSecretKey}`, {
-                "room_code": roomId, "socket_id": socket.id,
-            }).then(value => {
-                io.to(roomId).emit("leave_room", value.data)
-            }).catch((error) => {
+        "room_code": roomId, "socket_id": socket.id,
+    }).then(value => {
+        io.to(roomId).emit("leave_room", value.data)
+    }).catch((error) => {
 
     })
     // io.to(roomId).emit("leave_room",{"status":"ok"})
@@ -97,41 +95,43 @@ io.on('connection', (socket) => {
 
     socket.on("message", (data) => {
         axios.post(`${baseUrl}/send-message?secret=${godfatherSecretKey}`, {
-                "room_code": data["room"],
-                "socket_id": socket.id,
-                "message":data["message"],
-                "receiver":data["receiver"]
-            }).then(value => {
-                if(value.data["status"] === "ok"){
-                    if(value.data["data"]["message"]["receiver"]){
-                        console.log("Send to",value.data["data"]["message"]["receiver"])
-                        io.to(`${value.data["data"]["message"]["receiver"]}`).emit("message",value.data["data"]["message"])
-                    }
-
+            "room_code": data["room"],
+            "socket_id": socket.id,
+            "message": data["message"],
+            "receiver": data["receiver"]
+        }).then(value => {
+            if (value.data["status"] === "ok") {
+                if (value.data["data"]["message"]["receiver"]) {
+                    console.log("Send to", value.data["data"]["message"]["receiver"])
+                    io.to(`${value.data["data"]["message"]["receiver"]}`).emit("message", value.data["data"]["message"])
                 }
-            }).catch( (error) => {
-                console.log(error)
-            })
+
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
 
     })
     socket.on("command", (data) => {
 
         if (socket.rooms.has(data["room"])) {
             axios.post(`${baseUrl}/command?secret=${godfatherSecretKey}`, {
-                "room_code": data["room"], "socket_id": socket.id,"command":data["command"],
+                "room_code": data["room"], "socket_id": socket.id, "command": data["command"],
                 "target_socket_id": ("target_socket_id" in data) ? data["target_socket_id"] : null
             }).then(value => {
-                if(data["command"] === "kick"){
-                    io.in(data["room"]).fetchSockets().then(s => {
-                        console.log(s.client.id,data["target_socket_id"])
-                        if(s.client.id === data["target_socket_id"]){
-                            s.leave(data["room"])
-                            s.disconnect()
+                if (data["command"] === "kick") {
+                    io.in(data["room"]).fetchSockets().then(sockets => {
+                        for (const s of sockets) {
+                            if (s.id === data["target_socket_id"]) {
+                                s.leave(data["room"])
+                                s.disconnect()
+                            }
                         }
+
                     })
                 }
                 io.to(data["room"]).emit("command", data)
-            }).catch( (error) => {
+            }).catch((error) => {
 
             })
 
@@ -140,10 +140,10 @@ io.on('connection', (socket) => {
     socket.on("players_info", (data) => {
         if (socket.rooms.has(data["room"])) {
             axios.post(`${baseUrl}/playerinfo?secret=${godfatherSecretKey}`, {
-                "room_code": data["room"], "socket_id": socket.id,"token":data["token"]
+                "room_code": data["room"], "socket_id": socket.id, "token": data["token"]
             }).then(value => {
                 io.to(data["room"]).emit("players_info", value.data)
-            }).catch( (error) => {
+            }).catch((error) => {
 
             })
         }
@@ -151,21 +151,21 @@ io.on('connection', (socket) => {
     })
 
     socket.on("load_messages", (data) => {
-        console.log("Rooom is:",data["room"])
+        console.log("Rooom is:", data["room"])
         if (socket.rooms.has(data["room"])) {
             axios.post(`${baseUrl}/messages?secret=${godfatherSecretKey}`, {
                 "room_code": data["room"], "socket_id": socket.id
             }).then(value => {
                 console.log(value.data)
                 socket.emit("load_messages", value.data)
-            }).catch( (error) => {
+            }).catch((error) => {
 
             })
         }
 
     })
 
-    socket.on("set_roles",(data) => {
+    socket.on("set_roles", (data) => {
         console.log(data)
         if (socket.rooms.has(data["room"])) {
 
@@ -173,20 +173,20 @@ io.on('connection', (socket) => {
                 "room_code": data["room"],
                 "socket_id": socket.id
             }).then(value => {
-                value.data["data"]["players"].forEach( (player) => {
+                value.data["data"]["players"].forEach((player) => {
                     console.log(player)
-                    if(player.room_role === "player"){
+                    if (player.room_role === "player") {
                         const socket_ins = io.sockets.sockets.get(player.socket_id);
 
-                        if(player.role.side === 0 && socket_ins){
+                        if (player.role.side === 0 && socket_ins) {
                             socket_ins.join(`mafia-${player.room__code}`)
                         }
 
                         io.to(player["socket_id"]).emit("role", player)
                     }
 
-                } )
-            }).catch( (error) => {
+                })
+            }).catch((error) => {
             })
 
         }
@@ -194,30 +194,30 @@ io.on('connection', (socket) => {
 
     socket.on("send_action", (data) => {
         axios.post(`${baseUrl}/send-action?secret=${godfatherSecretKey}`, {
-                "room_code": data["room"],
-                "socket_id": socket.id,
-                "targets":data["targets"]
-            }).then(value => {
-                if(value.data["status"] === "ok"){
-                    io.to(`god-${data["room"]}`).emit("actions",value.data["data"])
-                }
-            }).catch( (error) => {
-                console.log(error)
-            })
+            "room_code": data["room"],
+            "socket_id": socket.id,
+            "targets": data["targets"]
+        }).then(value => {
+            if (value.data["status"] === "ok") {
+                io.to(`god-${data["room"]}`).emit("actions", value.data["data"])
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
 
     })
 
     socket.on("get_actions", (data) => {
         axios.post(`${baseUrl}/actions?secret=${godfatherSecretKey}`, {
-                "room_code": data["room"],
-                "socket_id": socket.id,
-            }).then(value => {
-                if(value.data["status"] === "ok"){
-                    io.to(`god-${data["room"]}`).emit("actions",value.data["data"])
-                }
-            }).catch( (error) => {
-                console.log(error)
-            })
+            "room_code": data["room"],
+            "socket_id": socket.id,
+        }).then(value => {
+            if (value.data["status"] === "ok") {
+                io.to(`god-${data["room"]}`).emit("actions", value.data["data"])
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
 
     })
 });
